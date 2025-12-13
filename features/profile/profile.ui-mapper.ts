ea21b73719ee166bb1e2ca0b9f63945251cb22d0
TAJEWUO <antonynjapit@gmail.com>
2025-12-14 00:04:26 +0300

    Update profile.ui-mapper.ts

--- features/profile/profile.ui-mapper.ts
+++ features/profile/profile.ui-mapper.ts
@@ -1,19 +1,13 @@
-import { Profile } from "./profile.types";
-
-export interface ProfileUI {
-  id: string;
-  fullName: string;
-  phone: string;
-  level: Profile["level"];
-  avatarUrl: string;
-}
+// features/profile/profile.ui-mapper.ts
+import { Profile } from "./profile.types"
+import { ProfileUI } from "./profile.ui-contract"
 
 export function mapProfileToUI(profile: Profile): ProfileUI {
   return {
     id: profile.id,
-    fullName: `${profile.firstName} ${profile.lastName}`,
+    name: `${profile.firstName} ${profile.lastName}`,
     phone: profile.phone,
     level: profile.level,
     avatarUrl: profile.profilePicture,
-  };
+  }
 }
