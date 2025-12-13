1a6f6b7da691e16ef7dc6e91880e1d4914904f40
TAJEWUO <antonynjapit@gmail.com>
2025-12-13 22:46:44 +0300

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,20 @@
+// lib/utils.ts
+
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+/**
+ * cn()
+ * ----
+ * Classname utility used by:
+ * - shadcn/ui components
+ * - Radix UI wrappers
+ * - dropdown-menu
+ * - button, card, input, etc.
+ *
+ * DO NOT move this function.
+ * DO NOT rename this file.
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
